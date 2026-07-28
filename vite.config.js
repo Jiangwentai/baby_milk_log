90fdd7092ba2198414b9a0184a136d1cde21942f
Jiangwentai <kwindboy@gmail.com>
2026-07-29 01:31:12 +0800

    fx

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: '/baby_milk_log/',
+  base: '/',
   plugins: [
     vue(),
     VitePWA({
